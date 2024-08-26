20a1d203586f2379934311260542e514526994a8
LakshyaGautam <gautamlakshya021@gmail.com>
2024-08-26 18:19:09 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password-Manager
+ This is a password manager app where you can add, update or delete your passwords for various websites.
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="bg-slate-800 text-white flex flex-col justify-center items-center w-full ">
+    <div className="logo font-bold text-white text-2xl">
+        <span className="text-green-500">&lt;</span>
+        Pass
+        <span className="text-green-500">OP/&gt;</span>
+        </div>
+
+    <div>
+      Created by Lakshya Gautam
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Footer
